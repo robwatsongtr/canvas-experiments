4b5c629b958fddd7eb22b03db11b0ef4af19cc52
robwatsongtr <rwatso@gmail.com>
2023-05-10 09:50:09 -0700

    renamed html file to 'index.html

---
+++ script.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+// four arguments for fillrect: x placement, y placement, widthOfRec, heightOfRec
+ctx.fillRect(20, 20, 100, 100)
