fa4a54d9fa04a0daa8efc29531336919f02a64d8
robwatsongtr <rwatso@gmail.com>
2023-05-13 21:25:53 -0700

    animate put into ball class

---
+++ reference/scriptOld.js
@@ -0,0 +1,78 @@
+// Link to HTML5 Canvas API Crash Course: 
+// https://www.youtube.com/watch?v=gm1QtePAYTM
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+// fillRect()
+// four arguments for fillrect: x placement, y placement, widthOfRec, heightOfRec
+// Can also use strokeRect() for non-filled rectangles. 
+
+// Row 1:
+ctx.fillStyle="blue"
+ctx.fillRect(30, 40, 150, 150)
+ctx.fillRect(220, 40, 160, 150)
+ctx.fillRect(420, 40, 150, 150)
+
+// Row 2
+ctx.fillStyle = "green"
+ctx.fillRect(30, 220, 150, 150)
+ctx.fillRect(220, 220, 160, 150)
+ctx.fillRect(420, 220, 150, 150)
+
+// Row 3
+ctx.fillStyle = "orange"
+ctx.fillRect(30, 410, 150, 150)
+ctx.fillRect(220, 410, 160, 150)
+ctx.fillRect(420, 410, 150, 150)
+
+const ball1 = {
+  x: 200,
+  y: 205,
+  size: 10, // radius 
+  dx: 5,  // the increment of movement on x axis
+  dy: 0  // the increment of movement on the y axis
+}
+
+// arc(x pos, y pos, radius, startAngle, endAngle)  to get 360 degrees its 2 * PI radians 
+function drawBall() {
+  ctx.beginPath()
+  ctx.arc(ball1.x, ball1.y, ball1.size, 0, Math.PI * 2)
+  ctx.fillStyle = 'purple'
+  ctx.fill()
+}
+
+function animateBall() {
+
+  // clear previous position of object along path  
+  //
+  // clearRect(x, y, width, height)
+  // x is the x axis cordinate of the rectangle's starting point
+  // y is the y axis of the coordinate's starting point
+  // width is the rectangle's width, height is rectangle's height
+  //
+  // The x and y arguments to clearRect() should be the coordinates of the top-left 
+  // corner of the rectangle to clear, minus the size of the ball, NOT the dx or dy
+  ctx.clearRect(ball1.x - ball1.size, ball1.y - ball1.size, ball1.size * 2, ball1.size * 2);
+
+  // change position:
+  ball1.x += ball1.dx 
+  ball1.y += ball1.dy
+
+  // draw ball   
+  drawBall()
+
+  if( ball1.x + (ball1.size * 2) > canvas.width || ball1.x - (ball1.size * 2) < 0 ) {
+    ball1.dx *= -1
+  }
+
+  // animate but adjust 'frame rate' with setTimeout, last argument is time between draws
+  // in milliseconds (1000 is one frame per second)
+  setTimeout( () => {
+   requestAnimationFrame(animateBall)
+  }, 5)
+
+}
+
+animateBall()
+
--- script.js
+++ script.js
@@ -19,117 +19,28 @@ const rectangles = [
   new Rectangle(420, 410, 150, 150, "orange"),
 ]
 
+// Draw rectangles 
 for( let rect of rectangles )  {
   rect.draw(ctx)
 }
 
-// Draw Ball 1, only move on x axis 
-// ( xpos, ypos, sizeRadius, color, dx, dy )
-const ball1 = new Ball( 200, 205, 10, "purple", 5, 0 )
+// Draw Ball 1,  move on x axis 
+// ( xpos, ypos, sizeRadius, color, dx, dy, animationFrameDelay )
+const ball1 = new Ball( 200, 205, 10, "purple", 5, 0, 20 )
 ball1.draw(ctx)
-
-// Animate Ball 1: -------------------
-function animateBall1() {
-  // clear previous position of object along path  
-  //
-  // clearRect(x, y, width, height)
-  // x is the x axis cordinate of the rectangle's starting point
-  // y is the y axis of the coordinate's starting point
-  // width is the rectangle's width, height is rectangle's height
-  //
-  // The x and y arguments to clearRect() should be the coordinates of the top-left 
-  // corner of the rectangle to clear, minus the size of the ball, NOT the dx or dy
-  ctx.clearRect(
-    ball1.xpos - ball1.sizeRadius, 
-    ball1.ypos - ball1.sizeRadius, 
-    ball1.sizeRadius * 2, 
-    ball1.sizeRadius * 2
-  );
-
-  // change position
-  ball1.xpos += ball1.dx 
-  ball1.ypos += ball1.dy
-
-  // draw ball
-  ball1.draw(ctx)
-
-  // left / right wall collision detection and reverse path 
-  if( ball1.xpos + (ball1.sizeRadius * 2) > canvas.width || ball1.xpos - (ball1.sizeRadius * 2) < 0 ) {
-    ball1.dx *= -1
-  }
-
-  // Animate but adjust 'frame rate' with setTimeout, last argument is time between draws
-  // in milliseconds (1000 is one frame per second)
-  setTimeout( () => {
-    requestAnimationFrame(animateBall1)
-   }, 15)
-}
-
-animateBall1()
+ball1.animateHoriz(ctx)
 
 // draw ball 2 only move on Y axis
-const ball2 = new Ball( 200, 30, 10, "red", 0, 5 )
+const ball2 = new Ball( 200, 30, 10, "red", 0, 5, 30 )
 ball2.draw(ctx)
+ball2.animateVert(ctx)
 
-function animateBall2() {
-  // clear previous position along path
-  ctx.clearRect(
-    ball2.xpos - ball2.sizeRadius, 
-    ball2.ypos - ball2.sizeRadius, 
-    ball2.sizeRadius * 2, 
-    ball2.sizeRadius * 2
-  );
-
-  // change position
-  ball2.xpos += ball2.dx 
-  ball2.ypos += ball2.dy
-
-  // draw ball
-  ball2.draw(ctx)
-
-  // left / right wall collision detection and reverse path 
-  if( ball2.ypos + (ball1.sizeRadius * 2) > canvas.height || ball2.ypos - (ball2.sizeRadius * 2) < 0 ) {
-    ball2.dy *= -1
-  }
-
-  // Animate but adjust 'frame rate' with setTimeout, last argument is time between draws
-  // in milliseconds (1000 is one frame per second)
-  setTimeout( () => {
-    requestAnimationFrame(animateBall2)
-   }, 10)
-}
-
-animateBall2()
-
-const ball3 = new Ball( 400, 30, 10, "gray", 0, 5 )
+// draw ball3 Y axis move 
+const ball3 = new Ball( 400, 30, 10, "green", 0, 5, 4)
 ball3.draw(ctx)
+ball3.animateVert(ctx)
 
-function animateBall3() {
-  // clear previous position along path
-  ctx.clearRect(
-    ball3.xpos - ball3.sizeRadius, 
-    ball3.ypos - ball3.sizeRadius, 
-    ball3.sizeRadius * 2, 
-    ball3.sizeRadius * 2
-  );
-
-  // change position
-  ball3.xpos += ball3.dx 
-  ball3.ypos += ball3.dy
-
-  // draw ball
-  ball3.draw(ctx)
-
-  // left / right wall collision detection and reverse path 
-  if( ball3.ypos + (ball3.sizeRadius * 2) > canvas.height || ball3.ypos - (ball3.sizeRadius * 2) < 0 ) {
-    ball3.dy *= -1
-  }
-
-  // Animate but adjust 'frame rate' with setTimeout, last argument is time between draws
-  // in milliseconds (1000 is one frame per second)
-  setTimeout( () => {
-    requestAnimationFrame(animateBall3)
-   }, 0)
-}
-
-animateBall3()
+// draw ball 3 x axis move 
+const ball4 = new Ball( 200, 390, 10, "brown", 5, 0, 13 )
+ball4.draw(ctx)
+ball4.animateHoriz(ctx)
