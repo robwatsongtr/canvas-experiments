4d7dcb0bdcd74dc5f35acfc1ca1750436bc02bcb
robwatsongtr <rwatso@gmail.com>
2023-05-21 20:41:13 -0700

    dir structure

--- src/script.js
+++ src/script.js
@@ -26,21 +26,21 @@ for( let rect of rectangles )  {
 
 // Draw Ball 1,  move on x axis 
 // ( xpos, ypos, sizeRadius, color, dx, dy, animationFrameDelay )
-const ball1 = new Ball( 200, 205, 10, "purple", 5, 0, 20 )
+const ball1 = new Ball( 200, 205, 10, "purple", 5, 0, 5 )
 ball1.draw(ctx)
 ball1.animateHoriz(ctx)
 
 // draw ball 2 only move on Y axis
-const ball2 = new Ball( 200, 30, 10, "red", 0, 5, 30 )
+const ball2 = new Ball( 200, 30, 10, "red", 0, 5, 2 )
 ball2.draw(ctx)
 ball2.animateVert(ctx)
 
-// draw ball3 Y axis move 
-const ball3 = new Ball( 400, 30, 10, "green", 0, 5, 4)
+// draw ball3, Y axis move 
+const ball3 = new Ball( 400, 30, 10, "green", 0, 5, 40)
 ball3.draw(ctx)
 ball3.animateVert(ctx)
 
-// draw ball 3 x axis move 
+// draw ball 4, x axis move 
 const ball4 = new Ball( 200, 390, 10, "brown", 5, 0, 13 )
 ball4.draw(ctx)
 ball4.animateHoriz(ctx)
